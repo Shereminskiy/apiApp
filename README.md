e218ca8b3f7a7b08a2583c5eb9a621c12fe86ac7
alex <aalex.prog@gmail.com>
2015-12-04 00:03:46 +0200

    Added vagrant config

---
+++ README.md
@@ -0,0 +1,7 @@
+https://gist.github.com/rrosiek/8190550
+
+http://docs.vagrantup.com/v2/vagrantfile/ssh_settings.html
+
+https://vesselinv.com/lemp-with-vagrant/
+
+http://www.dev-metal.com/super-simple-vagrant-lamp-stack-bootstrap-installable-one-command/
---
+++ provision.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+DBPASSWD=1q2w3e4r
+DBNAME=dbname
+DBUSER=dbuser
+
+sudo aptitude update -q
+
+# Force a blank root password for mysql
+echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
+
+# Install mysql, nginx, php5-fpm
+sudo aptitude install -q -y -f mysql-server mysql-client nginx php5-fpm mc vim curl
+
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
+
+
+
+# Install commonly used php packages
+sudo aptitude install -q -y -f php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
+
+sudo rm /etc/nginx/sites-available/default
+sudo touch /etc/nginx/sites-available/default
+
+sudo cat >> /etc/nginx/sites-available/default <<'EOF'
+server {
+  listen   80;
+
+  root /usr/share/nginx/html;
+  index index.php index.html index.htm;
+
+  # Make site accessible from http://localhost/
+  server_name _;
+
+  location / {
+    # First attempt to serve request as file, then
+    # as directory, then fall back to index.html
+    try_files $uri $uri/ /index.html;
+  }
+
+  location /doc/ {
+    alias /usr/share/doc/;
+    autoindex on;
+    allow 127.0.0.1;
+    deny all;
+  }
+
+  # redirect server error pages to the static page /50x.html
+  #
+  error_page 500 502 503 504 /50x.html;
+  location = /50x.html {
+    root /usr/share/nginx/html;
+  }
+
+  # pass the PHP scripts to FastCGI server listening on /tmp/php5-fpm.sock
+  #
+  location ~ \.php$ {
+    try_files $uri =404;
+    fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    fastcgi_pass unix:/var/run/php5-fpm.sock;
+    fastcgi_index index.php;
+    include fastcgi_params;
+  }
+
+  # deny access to .htaccess files, if Apache's document root
+  # concurs with nginx's one
+  #
+  location ~ /\.ht {
+    deny all;
+  }
+
+  ### phpMyAdmin ###
+  location /phpmyadmin {
+    root /usr/share/;
+    index index.php index.html index.htm;
+    location ~ ^/phpmyadmin/(.+\.php)$ {
+      client_max_body_size 4M;
+      client_body_buffer_size 128k;
+      try_files $uri =404;
+      root /usr/share/;
+
+      # Point it to the fpm socket;
+      fastcgi_pass unix:/var/run/php5-fpm.sock;
+      fastcgi_index index.php;
+      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+      include /etc/nginx/fastcgi_params;
+    }
+
+    location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)) {
+      root /usr/share/;
+    }
+  }
+  location /phpMyAdmin {
+    rewrite ^/* /phpmyadmin last;
+  }
+  ### phpMyAdmin ###
+}
+EOF
+
+sudo touch /usr/share/nginx/html/info.php
+sudo cat >> /usr/share/nginx/html/info.php <<'EOF'
+<?php phpinfo(); ?>
+EOF
+
+echo -e "\n--- Install MySQL specific packages and settings ---\n"
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+
+sudo aptitude install -q -y -f phpmyadmin
+
+sudo service nginx restart
+
+sudo service php5-fpm restart
+
+# Installing Composer for PHP package management
+#echo -e "\n--- Installing Composer for PHP package management ---\n"
+#curl --silent https://getcomposer.org/installer | php > /dev/null 2>&1
+#mv composer.phar /usr/local/bin/composer
+
+#Installing NodeJS and NPM
+#echo -e "\n--- Installing NodeJS and NPM ---\n"
+#apt-get -y install nodejs > /dev/null 2>&1
+#curl --silent https://npmjs.org/install.sh | sh > /dev/null 2>&1
